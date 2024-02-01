1819c58b3f1fb62540f59309261033ccd13ed238
Farhad <farhad.szd@gmail.com>
2024-02-01 19:11:09 +0400

    added author option

--- git_stash.sh
+++ git_stash.sh
@@ -3,13 +3,15 @@
 # Default values
 SEARCH_DIR=$(pwd)
 START_DATE="2024-01-01"
+AUTHOR=""  # Replace with a default author name or leave empty
 
 # Parse named parameters
-while getopts d:s: flag
+while getopts d:s:a: flag
 do
     case "${flag}" in
         d) SEARCH_DIR=${OPTARG};;
         s) START_DATE=${OPTARG};;
+        a) AUTHOR=${OPTARG};;
     esac
 done
 
@@ -18,12 +20,13 @@ total_add=0
 while IFS= read -r repo; do 
     echo "Repository: ${repo}"
     
-    add=$(cd "${repo}/.." && git log --author="Farhad" --since="$START_DATE" --pretty=tformat: --numstat | gawk '{ add += $1 } END { print add }')
+    add=$(cd "${repo}/.." && git log --author="$AUTHOR" --since="$START_DATE" --pretty=tformat: --numstat | gawk '{ add += $1 } END { print add }')
 
     total_add=$((total_add + add))
     
-    echo "Added lines in this repo: $add"
+    echo "Added lines in this repo by $AUTHOR: $add"
     echo
 done < <(find "$SEARCH_DIR" -type d -name .git) 
 
-echo "Total added lines across all repositories since $START_DATE: $total_add"
+echo "Total added lines across all repositories by $AUTHOR since $START_DATE: $total_add"
+
